30108cd9ab3216dba56d67dce7fcef6a570db808
Mirko Abbrescia <mirko.abbrescia@gmail.com>
2023-12-20 11:03:46 +0100

    Added efashion customize

--- src/router/IndexPage.tsx
+++ src/router/IndexPage.tsx
@@ -4,6 +4,7 @@ function IndexPage() {
         <div className="flex flex-col items-center justify-center h-full">
             <span className="text-5xl font-bold">Reactor</span>
             <span className="text-5xl font-bold">Management</span>
+            <span className="text-5xl font-bold">Efashion</span>
         </div>
     </div>
   )
